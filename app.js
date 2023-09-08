b37d6334785bda67a6e21bd71d9cf6547853cb5d
vikroriya_masyuk <viktoriyamasyuk@yandex.ru>
2023-09-08 09:11:57 +0000

    cors fixed

--- app.js
+++ app.js
@@ -15,8 +15,7 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(helmet());
-app.use(cors);
+app.use(helmet({crossOriginEmbedderPolicy: false, crossOriginResourcePolicy: false}));
 
 app.get('/crash-test', () => {
   setTimeout(() => {
